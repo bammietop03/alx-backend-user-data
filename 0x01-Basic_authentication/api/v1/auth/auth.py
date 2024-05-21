9c27d7d9bdbe00121d1c47bf967b896763fd4156
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-21 23:44:48 +0100

    Baic_authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,26 +15,26 @@ class Auth:
         if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        l_path = len(path)
-        if l_path == 0:
+        path_length = len(path)
+        if path_length == 0:
             return True
 
-        slash_path = True if path[l_path - 1] == '/' else False
+        slash_path = True if path[path_length - 1] == '/' else False
 
         tmp_path = path
         if not slash_path:
             tmp_path += '/'
 
         for exc in excluded_paths:
-            l_exc = len(exc)
-            if l_exc == 0:
+            exc_length = len(exc)
+            if exc_length == 0:
                 continue
 
-            if exc[l_exc - 1] != '*':
+            if exc[exc_length - 1] != '*':
                 if tmp_path == exc:
                     return False
             else:
-                if exc[:-1] == path[:l_exc - 1]:
+                if exc[:-1] == path[:exc_length - 1]:
                     return False
 
         return True
