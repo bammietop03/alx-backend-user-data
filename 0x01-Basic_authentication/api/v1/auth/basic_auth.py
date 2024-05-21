084752dbd8c473d08c74b3f3576c46df6ea686d9
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-21 22:57:52 +0100

    Baic_authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from .auth import Auth
 import requests
 from typing import List, TypeVar
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -62,3 +63,46 @@ class BasicAuth(Auth):
             return separate[0], separate[1]
         else:
             return None, None
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password."""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_email, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """overloads Auth and retrieves the User instance for a request"""
+        if request is None:
+            return None
+
+        base64_token = self.authorization_header(request)
+        if base64_token is None:
+            return None
+        extract_token = self.extract_base64_authorization_header(base64_token)
+        if extract_token is None:
+            return None
+        decoded_token = self.decode_base64_authorization_header(extract_token)
+        if decoded_token is None:
+            return None
+        user_details = self.extract_user_credentials(decoded_token)
+        if base64_token is (None, None):
+            return None
+        user_object = self.user_object_from_credentials(user_details[0],
+                                                        user_details[1])
+        if user_object is None:
+            return None
+        return user_object
