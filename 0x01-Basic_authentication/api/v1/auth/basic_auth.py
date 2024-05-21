2f0818864e2cb4df4c220cf093345c3cd07ccc98
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-21 22:07:41 +0100

    Baic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ API authentication
 from .auth import Auth
 import requests
 from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -23,3 +24,41 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header,
+                                             validate=True)
+
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ returns the user email and password from
+            the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' in decoded_base64_authorization_header:
+            separate = decoded_base64_authorization_header.split(':')
+            return separate[0], separate[1]
+        else:
+            return None, None
