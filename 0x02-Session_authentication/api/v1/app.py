03266c3f5a1f0e723e7b3f323317ef202388df02
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-23 19:01:34 +0100

    Session Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,9 +54,9 @@ def check_auth():
     path_list = ['/api/v1/status/', '/api/v1/unauthorized/',
                  '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, path_list):
-        if auth.authorization_header(request) is None:
-            abort(401)
         if auth.session_cookie(request) is None:
+            abort(401)
+        if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
