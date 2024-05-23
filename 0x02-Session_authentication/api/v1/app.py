07abd664dd7b27076d5ba454f10ef2068f02c237
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-23 19:05:29 +0100

    Session Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,20 +61,6 @@ def check_auth():
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
-    # if auth is None:
-    #     pass
-    # else:
-    #     setattr(request, "current_user", auth.current_user(request))
-    #     excluded_list = ['/api/v1/status/',
-    #                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
-    #                      '/api/v1/auth_session/login/']
-
-    #     if auth.require_auth(request.path, excluded_list):
-    #         cookie = auth.session_cookie(request)
-    #         if auth.authorization_header(request) is None and cookie is None:
-    #             abort(401, description="Unauthorized")
-    #         if auth.current_user(request) is None:
-    #             abort(403, description='Forbidden')
 
 
 if __name__ == "__main__":
