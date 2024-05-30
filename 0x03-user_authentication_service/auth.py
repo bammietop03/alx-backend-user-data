2b22594249bf0e3497da78aacb5cef7fdd35036e
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-30 20:39:15 +0100

    User Auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,16 @@ class Auth:
             return True
         else:
             return False
+
+    def create_session(self, email: str) -> str:
+        """  It takes an email string argument and
+        returns the session ID as a string."""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
