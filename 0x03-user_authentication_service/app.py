72d87bd8fae143b982af6268fd56aad813c650a3
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-30 19:32:33 +0100

    User Auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 """ Basic Flask App """
 from flask import Flask, request, jsonify
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        if user:
+            return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
